8f4bbc35488a1e3e39e1a30916e13142e275379a
Evan Stites-Clayton <stitesclayton@gmail.com>
2022-04-15 23:15:58 -0400

    Adds maximum words and declaration copy

--- pages/textsel.tsx
+++ pages/textsel.tsx
@@ -5,10 +5,21 @@ import Layout from "../c/Layout";
 import _ from "lodash";
 
 const blurb =
-  "This is just some text to use for the sampling and selection of stuff. Don't take it too serious. In fact, it's not even that great, it's just a bunch of words strung together, to make sentences, which in turn form paragraphs. Those paragraphs make up a narrative, and that narrative is my life.";
+  "We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.--That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, --That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security.--Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world.";
 
-const MAX_SELECTIONS = 3;
-const SEL_COLORS = ["#BECBD7", "#D7BED0", "#DFA39F"];
+const MAX_SELECTIONS = 9;
+const SEL_COLORS = [
+  "#BECBD7",
+  "#D7BED0",
+  "#DFA39F",
+  "#BECBD7",
+  "#D7BED0",
+  "#DFA39F",
+  "#BECBD7",
+  "#D7BED0",
+  "#DFA39F",
+];
+const MAX_WORDS = 24;
 
 const TextSel: NextPage = () => {
   const initSelections = Array(MAX_SELECTIONS).fill([-1, -1]);
@@ -108,6 +119,16 @@ const TextSel: NextPage = () => {
     [findSelection]
   );
 
+  const getWordsSelectedCount = React.useCallback(() => {
+    let wordsSelected = 0;
+    for (let i = 0; i < MAX_SELECTIONS; i++) {
+      if (selections[i][0] >= 0) {
+        wordsSelected += 1 + selections[i][1] - selections[i][0];
+      }
+    }
+    return wordsSelected;
+  }, [selections]);
+
   const onClickWord = React.useCallback(
     (wordIndex: number) => {
       if (selectionState == "nothing_selected") {
@@ -117,11 +138,22 @@ const TextSel: NextPage = () => {
           return;
         }
 
+        if (getWordsSelectedCount() > MAX_WORDS) {
+          // Don't start a new selection if you already have too many words.
+          return;
+        }
+
         assignSelection(selIndex, [wordIndex, wordIndex]);
         setSelectionState("first_selected");
         return;
       }
       if (selectionState == "first_selected") {
+        // Don't complete selection if it would exceed max words count.
+        const diff = wordIndex - selections[selIndex][0];
+        if (Math.abs(diff) + getWordsSelectedCount() > MAX_WORDS) {
+          return;
+        }
+
         if (wordIndex > selections[selIndex][0]) {
           if (
             isOverlappingInterval(selIndex, selections[selIndex][0], wordIndex)
@@ -147,6 +179,7 @@ const TextSel: NextPage = () => {
       assignSelection,
       cancelSelection,
       findSelection,
+      getWordsSelectedCount,
       isOverlappingInterval,
       selIndex,
       selectionState,
@@ -179,6 +212,26 @@ const TextSel: NextPage = () => {
     });
   }, [getBgColorForSelection, onClickWord, words]);
 
+  const renderConcat = () => {
+    let concat = "";
+
+    //sort them.
+    const sortedSelections = _.sortBy(
+      [...selections],
+      (selection: any) => selection[0]
+    );
+
+    sortedSelections.forEach((selection: any) => {
+      if (selection[0] >= 0) {
+        const wordsFromSel = words.slice(selection[0], selection[1] + 1);
+        concat += wordsFromSel.join(" ");
+        concat += " ";
+      }
+    });
+
+    return concat;
+  };
+
   return (
     <Layout>
       <div
@@ -188,7 +241,15 @@ const TextSel: NextPage = () => {
           alignItems: "center",
         }}
       >
-        <div style={{ maxWidth: 400 }}>{renderWords()}</div>
+        <div style={{ maxWidth: 600 }}>
+          <div>{renderWords()}</div>
+          <div style={{ marginTop: 12, fontWeight: "bold" }}>
+            {renderConcat()}
+          </div>
+          <div style={{ marginTop: 12 }}>
+            Words Remaining: {MAX_WORDS - getWordsSelectedCount()}
+          </div>
+        </div>
       </div>
     </Layout>
   );
